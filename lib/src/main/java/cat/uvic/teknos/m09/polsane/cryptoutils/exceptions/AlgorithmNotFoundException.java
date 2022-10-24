54b0d0c268bbc0bd2e6d87b842e3f41b23a65f00
pol sañé jové <pol.sane@uvic.cat>
2022-10-24 09:31:32 +0200

    I have added an exception for when the algorithm of cryptoutils isnt found

---
+++ lib/src/main/java/cat/uvic/teknos/m09/polsane/cryptoutils/exceptions/AlgorithmNotFoundException.java
@@ -0,0 +1,22 @@
+package cat.uvic.teknos.m09.polsane.cryptoutils.exceptions;
+
+public class AlgorithmNotFoundException extends RuntimeException{
+    public AlgorithmNotFoundException() {
+    }
+
+    public AlgorithmNotFoundException(String message) {
+        super(message);
+    }
+
+    public AlgorithmNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AlgorithmNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public AlgorithmNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
