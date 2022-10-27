96161509a364c0f13a601ef1186469f7960e4bbe
pol sañé jové <pol.sane@uvic.cat>
2022-10-27 21:31:10 +0200

    added javadoc comments to algorithmNotFounException and CryptoUtilsPropertiesException

--- lib/src/main/java/cat/uvic/teknos/m09/polsane/cryptoutils/exceptions/AlgorithmNotFoundException.java
+++ lib/src/main/java/cat/uvic/teknos/m09/polsane/cryptoutils/exceptions/AlgorithmNotFoundException.java
@@ -5,24 +5,42 @@ package cat.uvic.teknos.m09.polsane.cryptoutils.exceptions;
  * @see java.lang.RuntimeException
  */
 public class AlgorithmNotFoundException extends RuntimeException{
-    /**
-     *
+    /**A constructor without parameters
      */
     public AlgorithmNotFoundException() {
     }
 
+    /**
+     * constructor with a message parameter
+     * @param message String that is the exception's message
+     */
     public AlgorithmNotFoundException(String message) {
         super(message);
     }
 
+    /**
+     * constructor with a message and a cause parameter
+     * @param message String that is the exception's message
+     * @param cause Throwable that is the exeception's cause
+     */
     public AlgorithmNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**constructor with a cause parameter
+     * @param cause Throwable that is the exeception's cause
+     */
     public AlgorithmNotFoundException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * constructor with a message, a cause parameter, a enableSuppresion parameter and a writableSuppression parameter
+     * @param message String that is the exception's message
+     * @param cause Throwable that is the exeception's cause
+     * @param enableSuppression boolean that is enableSuppression
+     * @param writableStackTrace boolean that is writableStackTrace
+     */
     public AlgorithmNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
--- lib/src/main/java/cat/uvic/teknos/m09/polsane/cryptoutils/exceptions/CryptoUtilsPropertiesException.java
+++ lib/src/main/java/cat/uvic/teknos/m09/polsane/cryptoutils/exceptions/CryptoUtilsPropertiesException.java
@@ -1,21 +1,43 @@
 package cat.uvic.teknos.m09.polsane.cryptoutils.exceptions;
 
+
+/**An exception class that extends RuntimeException and is called when the properties attribute of CryptoUtils.java cannot find a cryptoutils.properties in the '/cryptoutils.properties' location
+ * @author pol
+ * @see java.lang.RuntimeException
+ */
 public class CryptoUtilsPropertiesException extends RuntimeException{
+    /**A constructor without parameters
+     */
     public CryptoUtilsPropertiesException() {
     }
-
+    /**
+     * constructor with a message parameter
+     * @param message String that is the exception's message
+     */
     public CryptoUtilsPropertiesException(String message) {
         super(message);
     }
-
+    /**
+     * constructor with a message and a cause parameter
+     * @param message String that is the exception's message
+     * @param cause Throwable that is the exeception's cause
+     */
     public CryptoUtilsPropertiesException(String message, Throwable cause) {
         super(message, cause);
     }
-
+    /**constructor with a cause parameter
+     * @param cause Throwable that is the exeception's cause
+     */
     public CryptoUtilsPropertiesException(Throwable cause) {
         super(cause);
     }
-
+    /**
+     * constructor with a message, a cause parameter, a enableSuppresion parameter and a writableSuppression parameter
+     * @param message String that is the exception's message
+     * @param cause Throwable that is the exeception's cause
+     * @param enableSuppression boolean that is enableSuppression
+     * @param writableStackTrace boolean that is writableStackTrace
+     */
     public CryptoUtilsPropertiesException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
